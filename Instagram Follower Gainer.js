5128ede5e2fd90b86e9a071ba5061ca843a6db1f
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2019-08-30 20:06:09 +0530

    Add files via upload

---
+++ Instagram Follower Gainer.js
@@ -0,0 +1,16 @@
+let i=4000
+    for (let index = 0; index < 200; index++) {
+ setTimeout(()=>{
+document.getElementsByClassName('BY3EC')[0].click()
+},i)
+i+=4000
+setTimeout(()=>{
+document.getElementsByClassName('BY3EC')[0].click()
+},i)
+i+=4000
+setTimeout(()=>{
+document.getElementsByClassName('aOOlW -Cab_   ')[0].click()
+},i)
+i+=4000
+
+    }
