1d17c3ab3f19fcd8a4d6d5eab53fb1280b867165
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2020-06-07 13:43:48 +0530

    Update Instagram Follower Gainer.js

--- Instagram Follower Gainer.js
+++ Instagram Follower Gainer.js
@@ -1,11 +1,11 @@
 let i=4000
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 2000; index++) {
  setTimeout(()=>{
 document.getElementsByClassName('BY3EC')[0].click()
 },i)
 i+=4000
 setTimeout(()=>{
-document.getElementsByClassName('BY3EC')[0].click()
+document.getElementsByClassName('glyphsSpriteFriend_Follow u-__7')[0].click()
 },i)
 i+=4000
 setTimeout(()=>{
